877bd649ae010a65af0814a7ecaa0afa2163f10a
Q Wade Billings <qwb@kynetx.com>
2024-06-05 09:16:52 -0400

    pulled out key name for EC2

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import { rds } from "./rds";
 import { eksVpc } from "./networking";
 import { eksCluster } from "./eksCluster";
+import { awsInstanceResource } from "./imdbEC2";
 
 // Export some values for use elsewhere
 export const kubeconfig = eksCluster.kubeconfig;
 export const vpcId = eksVpc.vpcId;
 export const rdsEndpoint = rds.endpoint;
 export const rdsPort = rds.port;
-export const ec2Instance = eksCluster.ec2Instance;
+export const ec2Instance = awsInstanceResource.id;
