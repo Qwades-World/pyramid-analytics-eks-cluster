21cae3f497c266e47a18669f9cc75beeeb4a2b8b
Q Wade Billings <qwb@kynetx.com>
2024-06-05 09:05:10 -0400

    added IMDB EC2 instance

---
+++ imdbEC2.ts
@@ -0,0 +1,64 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { eksVpc, securityGroup } from "./networking";
+
+const config = new pulumi.Config();
+const dbName = config.get("rdsDatabase");
+
+const ubuntu = aws.ec2.getAmi({
+  mostRecent: true,
+  filters: [
+    {
+      name: "name",
+      values: ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"],
+    },
+    {
+      name: "virtualization-type",
+      values: ["hvm"],
+    },
+  ],
+  owners: ["099720109477"],
+});
+
+const ebsVol = new aws.ebs.Volume("pyramid-imdb-001", {
+  availabilityZone: "us-east-1b",
+  finalSnapshot: true,
+  size: 100,
+  tags: {
+      Name: "Pyramid-IMDB-Volume-001",
+  },
+});
+
+export const awsInstanceResource = new aws.ec2.Instance("pyramid-imdb-instance", {    
+    ami: ubuntu.then((ubuntu) => ubuntu.id),
+    associatePublicIpAddress: false,
+    availabilityZone: "us-east-1b",
+    disableApiStop: false,
+    disableApiTermination: true,
+    ebsOptimized: true,
+    instanceType: "t3.2xlarge",
+    keyName: "deployer-wade-000",
+    monitoring: true,
+    rootBlockDevice: {
+        deleteOnTermination: false,
+        deviceName: "/dev/sda1",
+        encrypted: false,
+        tags: {
+            string: "env:production",
+        },
+        volumeSize: 50,
+        volumeType: "gp3",
+    },
+    subnetIds: eksVpc.privateSubnetIds,
+    tags: {
+        string: "string",
+    },
+    vpcSecurityGroupIds: [securityGroup.id],
+});
+
+const ebsAtt = new aws.ec2.VolumeAttachment("pyramid-imdb-001-attach", {
+  deviceName: "/dev/sdh",
+  volumeId: ebsVol.id,
+  instanceId: awsInstanceResource.id,
+  stopInstanceBeforeDetaching: true,
+});
