b49badfc1c77fef131d93ae3fc0fba76f64595ce
Q. Wade Billings <qwbillings@gmail.com>
2024-06-08 21:38:43 -0400

    Formatting and cleanup

--- eksCluster.ts
+++ eksCluster.ts
@@ -4,27 +4,27 @@ import { eksVpc } from "./networking";
 
 // Grab some values from the Pulumi configuration (or use default values)
 const config = new pulumi.Config();
-const minClusterSize = config.getNumber("minClusterSize") || 3;
-const maxClusterSize = config.getNumber("maxClusterSize") || 6;
 const desiredClusterSize = config.getNumber("desiredClusterSize") || 3;
 const eksNodeInstanceType = config.get("eksNodeInstanceType") || "t3.medium";
+const maxClusterSize = config.getNumber("maxClusterSize") || 6;
+const minClusterSize = config.getNumber("minClusterSize") || 3;
 
 // Create the EKS cluster
 export const eksCluster = new eks.Cluster("pyramid-eks-cluster", {
-    vpcId: eksVpc.vpcId,
-    publicSubnetIds: eksVpc.publicSubnetIds,
-    privateSubnetIds: eksVpc.privateSubnetIds,
-    instanceType: eksNodeInstanceType,
     desiredCapacity: desiredClusterSize,
-    minSize: minClusterSize,
-    maxSize: maxClusterSize,
-    nodeAssociatePublicIpAddress: false,
-    endpointPrivateAccess: false,
-    endpointPublicAccess: true,
     enabledClusterLogTypes: [
         "api",
         "audit",
         "authenticator",
     ],
+    endpointPrivateAccess: false,
+    endpointPublicAccess: true,
+    instanceType: eksNodeInstanceType,
+    maxSize: maxClusterSize,
+    minSize: minClusterSize,
+    nodeAssociatePublicIpAddress: false,
+    privateSubnetIds: eksVpc.privateSubnetIds,
+    publicSubnetIds: eksVpc.publicSubnetIds,
     version: "1.30",
+    vpcId: eksVpc.vpcId,
 });
--- networking.ts
+++ networking.ts
@@ -4,21 +4,20 @@ import * as awsx from "@pulumi/awsx";
 
 // Retrieve configuration values with defaults
 const config = new pulumi.Config();
-const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
 const extSshAccess = config.get("extSshAccess") || "0.0.0.0/0";
 const intSshAccess = config.get("intSshAccess") || "192.168.0.0/16";
+const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
 
 // Create a new VPC to host the EKS cluster
 export const eksVpc = new awsx.ec2.Vpc("pyramid-eks-vpc", {
-    enableDnsHostnames: true,
     availabilityZoneNames: ["us-east-1a", "us-east-1b", "us-east-1c"],
     cidrBlock: vpcNetworkCidr,
+    enableDnsHostnames: true,
     subnetStrategy: awsx.ec2.SubnetAllocationStrategy.Legacy,
 });
 
 // Create a security group that allows access to the RDS instance from the EKS cluster
 export const securityGroupRDS = new aws.ec2.SecurityGroup("pyramid-rds-sg", {
-    vpcId: eksVpc.vpcId,
     description: "Allow cluster access to rds instance",
     ingress: [
         { protocol: "tcp", fromPort: 5432, toPort: 5432, cidrBlocks: [vpcNetworkCidr] }, // Allow PostgreSQL access
@@ -30,11 +29,11 @@ export const securityGroupRDS = new aws.ec2.SecurityGroup("pyramid-rds-sg", {
         Name: "pyramid-rds-security-group",
         Environment: "production",
     },
+    vpcId: eksVpc.vpcId,
 });
 
 // Create a security group that allows SSH access to the EC2 instanc
 export const securityGroupEC2 = new aws.ec2.SecurityGroup("pyramid-ec2-sg", {
-    vpcId: eksVpc.vpcId,
     description: "Allow external SSH access to EC2",
     ingress: [
         { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: [extSshAccess] }, // Allow SSH access from the internet
@@ -47,4 +46,5 @@ export const securityGroupEC2 = new aws.ec2.SecurityGroup("pyramid-ec2-sg", {
         Name: "pyramid-ec2-security-group",
         Environment: "production",
     },
+    vpcId: eksVpc.vpcId,
 });
