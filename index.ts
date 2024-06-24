1a972f492c74e301e48d1e35698605c57aa6dc2b
Q. Wade Billings <qwbillings@gmail.com>
2024-06-24 10:41:54 -0400

    Got the windows instances working

--- index.ts
+++ index.ts
@@ -2,24 +2,26 @@ import {
     awsIMDBInstance,
     awsGCADInstance,
     awsGCGROUPInstance,
-    awsGCDEALERInstance,
-    ebsAtt
+    awsGCDEALERInstance
 } from "./imdbEC2";
+
 import {
     eksCluster
 } from "./eksCluster";
+
 import {
     eksVpc
 } from "./networking";
+
 import {
     rds
 } from "./rds";
 
 // Export some values for use elsewhere
-export const gcadadminpasswd = awsGCADInstance.passwordData;
-export const gcgroupadpasswd = awsGCDEALERInstance.passwordData;
-export const gcdealeradminpasswd = awsGCGROUPInstance.passwordData;
-export const ec2imdbinstance = awsIMDBInstance.id;
+export const gcadadminpasswd = awsGCADInstance.publicDns;
+export const gcgroupadpasswd = awsGCDEALERInstance.publicDns;
+export const gcdealeradminpasswd = awsGCGROUPInstance.publicDns;
+export const ec2IMDBInstance = awsIMDBInstance.publicDns;
 export const kubeconfig = eksCluster.kubeconfig;
 export const rdsEndpoint = rds.endpoint;
 export const rdsPort = rds.port;
