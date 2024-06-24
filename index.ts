0cca8900d26c4014f2b15aabf3c02bc3491eaa70
Q. Wade Billings <qwbillings@gmail.com>
2024-06-24 08:09:51 -0400

    Housekeeping and formatting

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
-import { awsIMDBInstance, awsGCADInstance, awsGCGROUPInstance, awsGCDEALERInstance, ebsAtt } from "./imdbEC2";
-import { eksCluster } from "./eksCluster";
-import { eksVpc } from "./networking";
-import { rds } from "./rds";
+import {
+    awsIMDBInstance,
+    awsGCADInstance,
+    awsGCGROUPInstance,
+    awsGCDEALERInstance,
+    ebsAtt
+} from "./imdbEC2";
+import {
+    eksCluster
+} from "./eksCluster";
+import {
+    eksVpc
+} from "./networking";
+import {
+    rds
+} from "./rds";
 
 // Export some values for use elsewhere
-export const ebsVolume = ebsAtt.id;
-export const ec2IMDBInstance = awsIMDBInstance.id;
 export const ec2GCADInstance = awsGCADInstance.passwordData;
-export const ec2GCGROUPInstance = awsGCGROUPInstance.passwordData;
 export const ec2GCDEALERInstance = awsGCDEALERInstance.passwordData;
+export const ec2GCGROUPInstance = awsGCGROUPInstance.passwordData;
+export const ec2IMDBInstance = awsIMDBInstance.id;
 export const kubeconfig = eksCluster.kubeconfig;
 export const rdsEndpoint = rds.endpoint;
 export const rdsPort = rds.port;
